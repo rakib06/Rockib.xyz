8c5f0a2b6082ab812458c9bc909cd9dc46cb5cde
rakib06 <six.rakib@gmail.com>
2020-09-10 06:30:53 +0600

    very simple navbar is added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ color: "black" }}>
-          Welcome to Rockib's World !!
-        </p>
 
-      </header>
-    </div >
-  );
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Rakibul Hasan",
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'Be Relentless',
+        subTitle: 'Projects the make a difference',
+        text: 'Checkout my projects below'
+      },
+      about: {
+        title: 'About Me'
+      },
+      contact: {
+        title: 'Let\'s Talk'
+      }
+
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand> Rakibul Hasan </Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Skills</Link>
+                <Link className="nav-link" to="/w">Projects</Link>
+                <Link className="nav-link" to="/w">Works</Link>
+                <Link className="nav-link" to="/w">Awards</Link>
+                <Link className="nav-link" to="/w">Youtube</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+
+          </Navbar>
+        </Container>
+      </Router>
+    );
+  }
 }
+
 
 export default App;
